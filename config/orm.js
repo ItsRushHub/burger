287dec81800417f08a95ebebc63054c5d8189031
Oran Rush <oranrush@Orans-MBP.lan1>
2019-03-21 16:54:53 -0500

    Update4

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,28 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
-
+var orm = {
+    selectAll: function(tableInput) {
+        var queryString = "SELECT * FROM ?? WHERE ?? =?";
+        connection.query(queryString,[tableInput], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function(table, cols, values) {
+        var queryString = "INSERT INTO" + table;
+        connection.query(queryString, [values], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(table, cols, values) {
+        var queryString = "UPDATE" + table;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+};
 
 
 
